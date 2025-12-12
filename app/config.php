6b63e54223a8499c2d91d43f4458ef293f8ce497
Wilma <wilmareistad@icloud.com>
2025-12-12 13:30:38 +0100

    added footer.php and added code in autoload, config and header

---
+++ app/config.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// This file contains a list of global configuration settings.
+
+return [
+    'database_path' => sprintf('sqlite:%s/database/database.db', __DIR__),
+];
