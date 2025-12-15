bbfe7de5acc146a9bf973293d153fbdc77de9ec8
Wilma <wilmareistad@icloud.com>
2025-12-15 09:35:35 +0100

    added a form for check in and check out

--- index.php
+++ index.php
@@ -6,12 +6,32 @@ $statement = $database->query('SELECT * FROM rooms');
 
 $rooms = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-?> <ul>
-    <?php
-    foreach ($rooms as $room) {
-    ?>
-        <li><?= $room['type']; ?> </li>
-    <?php
-    }
-    ?>
-</ul>
+?>
+
+<form method="post" action="booking.php">
+    <?php foreach ($rooms as $room): ?>
+        <label>
+            <input type="radio" name="room_id" value="<?= $room['id'] ?>" required>
+            <?= htmlspecialchars($room['type']) ?> –
+            <?= (int)$room['price'] ?> $/natt
+        </label>
+    <?php endforeach; ?>
+
+    <label>
+        Check-in:
+        <input type="date"
+            name="arrival-date"
+            min="2026-01-01"
+            max="2026-01-31" required>
+    </label>
+
+    <label>
+        Check-out:
+        <input type="date"
+            name="departure_date"
+            min="2026-01-01"
+            max="2026-01-31" required>
+    </label>
+
+    <button type="submit">Boka rum</button>
+</form>
