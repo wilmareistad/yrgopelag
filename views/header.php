01d9693a9e0576b9c96c6622aec8b2eb9c8d3c5a
Wilma <wilmareistad@icloud.com>
2025-12-18 10:14:19 +0100

    added font

--- views/header.php
+++ views/header.php
@@ -9,11 +9,21 @@
     <link rel="stylesheet" href="/assets/styles/index.css">
     <link rel="stylesheet" href="/assets/styles/calender.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
 
     <nav>
         <a href="/index.php"><img class="logo" src="/assets/images/logo.jpeg"></a>
+        <a class="logo" href="/index.php">Scubaland</a>
         <a href="/index.php">Start</a>
     </nav>
